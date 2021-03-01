c43d46b61b93b6d945ed34ea72f0c4b424e104e8
xuqiang1227 <kairi1227@126.com>
2021-03-01 16:23:35 +0800

    feat: minus // need todo

--- src/bigNumber.ts
+++ src/bigNumber.ts
@@ -3,8 +3,23 @@ const isNegative = (a: string, b: string) => {
     || !a.startsWith('-') && b.startsWith('-');
 }
 
+const getOperation = (a: string, b: string) => {
+  if (a.startsWith('-') && b.startsWith('-')) {
+    return '--';
+  }
+  if (a.startsWith('-') && !b.startsWith('-')) {
+    return '-+';
+  }
+  if (!a.startsWith('-') && !b.startsWith('-')) {
+    return '++';
+  }
+  if (!a.startsWith('-') && b.startsWith('-')) {
+    return '+-';
+  }
+}
 
-const longAdd = (a: string, b: string, carry: number = 0): [string, number] => {
+// 加
+const longPlus = (a: string, b: string, carry: number = 0): [string, number] => {
   let _tempA = a.split('');
   let _tempB = b.split('');
   if (_tempA.length < _tempB.length) {
@@ -23,10 +38,48 @@ const longAdd = (a: string, b: string, carry: number = 0): [string, number] => {
   return [sum, carry];
 }
 
-export const add = (a: string, b: string) => {
-  const sub = isNegative(a, b);
-  if (sub) {
-    //TODO 减法
+const floatPlus = (a: string, b: string) => {
+  const isFloat = a.includes('.') || b.includes('.');
+  const [a1 = "0", a2 = "0"] = a.split('.');
+  const [b1 = "0", b2 = "0"] = b.split('.');
+  const [float, carry] = isFloat ? longPlus(a2, b2, 0) : ["", 0];
+  let [num, _carry] = longPlus(a1, b1, carry);
+  if (_carry > 0) {
+    num = `${_carry}${num}`;
+  }
+  return `${num}${isFloat ? `.${float}` : ''}`;
+}
+
+const floatMinus = (a: string, b: string) => {
+  const isFloat = a.includes('.') || b.includes('.');
+  let [a1 = "0", a2 = "0"] = a.split('.');
+  const [b1 = "0", b2 = "0"] = b.split('.');
+  const [float, borrow] = isFloat ? longMinus(a2, b2) : ["", false];
+  if (borrow) {
+    let arrayA1 = a1.split('');
+    let index = arrayA1.length - 1;
+    let _a = Number(arrayA1[index]) - 1;
+    arrayA1[index] = String(_a);
+    while (_a < 0) {
+      arrayA1[index--] = "0";
+      _a = Number(a1[index]) - 1;
+    }
+    a1 = arrayA1.join('');
+  }
+  let res = longMinus(a1, b1);
+  return `${res[0]}${isFloat ? `.${float}` : ''}`
+}
+
+export const plus = (a: string, b: string) => {
+  const _sub = isNegative(a, b);
+  if (_sub) {
+    const op = getOperation(a, b);
+    switch (op) {
+      case "-+":
+        return minus(b.replace('-', ''), a.replace('-', ''));
+      default:
+        return minus(a.replace('-', ''), b.replace('-', ''));
+    }
   }
 
   const isSubAdd = a.startsWith('-') && b.startsWith('-');
@@ -35,18 +88,63 @@ export const add = (a: string, b: string) => {
     b = b.replace('-', '');
   }
 
-  const isFloat = a.includes('.') || b.includes('.');
-  const [a1 = "0", a2 = "0"] = a.split('.');
-  const [b1 = "0", b2 = "0"] = b.split('.');
-  const [float, carry] = isFloat ? longAdd(a2, b2, 0) : ["", 0];
-  let [num, _carry] = longAdd(a1, b1, carry);
-  if (!isFloat && _carry > 0) {
-    num = `${_carry}${num}`;
-  }
-  return `${isSubAdd ? '-' : ''}${num}${isFloat ? `.${float}` : ''}`
+  return `${isSubAdd ? '-' : ''}${floatPlus(a, b)}`
 }
 
-const longPlus = (_a: string, _b: string): string => {
+// TODO
+const longMinus = (sub1: string, sub2: string) => {
+  let negative = sub1.length < sub2.length || (sub1.length === sub2.length && sub1.localeCompare(sub2) < 0);
+  if (negative) {
+    const _temp = sub1;
+    sub1 = sub2;
+    sub2 = _temp;
+  }
+  const a = sub1.split('');
+  const b = sub2.split('');
+  let _borrow: boolean = false;
+  const result = a.reduceRight((res: number[], cur: string, index: number) => {
+    const _b = Number(b.pop());
+    let _a = Number(cur);
+    if (_b > _a) {
+      let borrowIndex = index;
+      let borrow = Number(a[borrowIndex - 1]) - 1;
+      a[borrowIndex - 1] = String(borrow);
+      while (borrow < 0) {
+        a[--borrowIndex] = "0";
+        if (borrowIndex === 0 && borrow < 0) {
+          _borrow = true;
+          a.shift();
+          break;
+        }
+        borrow = Number(a[borrowIndex - 1]) - 1;
+      }
+      _a += 10;
+    }
+    res.unshift(_a - _b);
+    return res;
+  }, []);
+
+  return [`${negative ? '-' : ''}${result.join('').replace(/^0*(?=\d)/g, '')}`, _borrow];
+}
+
+export const minus = (a: string, b: string) => {
+  const _a = a.replace('-', '');
+  const _b = b.replace('-', '');
+  const op = getOperation(a, b);
+  switch (op) {
+    case '-+':
+      return `-${floatPlus(_a, _b)}`;
+    case '--':
+      return floatMinus(_b, _a);
+    case '+-':
+      return floatPlus(_a, _b);
+    default:
+      return floatMinus(_a, _b);
+  }
+}
+
+// 乘
+const longTimes = (_a: string, _b: string): string => {
   const a = _a.split('');
   const b = _b.split('');
   let sum = []; // ['111', '1110', '11100']
@@ -67,17 +165,17 @@ const longPlus = (_a: string, _b: string): string => {
   }, []);
 
   return sum.reduce((res, i) => {
-    const [sum, _c] = longAdd(res, String(i), 0);
+    const [sum, _c] = longPlus(res, String(i), 0);
     return `${_c > 0 ? _c : ''}${sum}`;
   }, '0');
 }
 
-export const plus = (a: string, b: string) => {
+export const times = (a: string, b: string) => {
   const _negative = isNegative(a, b);
   const float = (a.split('.')[1] || '').length + (b.split('.')[1] || '').length;
   a = a.replace(/\.|-/g, '');
   b = b.replace(/\.|-/g, '');
-  const _plus = longPlus(a, b);
-  return `${_negative ? '-' : ''}${float > 0 ? _plus.replace(new RegExp(`\\B(?!\\d{${float + 1}})`), '.') : _plus}`
+  const _times = longTimes(a, b);
+  return `${_negative ? '-' : ''}${float > 0 ? _times.replace(new RegExp(`\\B(?!\\d{${float + 1}})`), '.') : _times}`
 }
 
--- test/bigNumber.test.ts
+++ test/bigNumber.test.ts
@@ -1,25 +1,46 @@
-import { add, plus } from '../src/bigNumber';
+import { plus, times, minus } from '../src/bigNumber';
 
-
-test("bigNumber-add", () => {
-  const a = '111111111111119';
-  const b = '11111111111111112';
-  expect(add(a, b)).toBe('11222222222222231');
-  expect(add('0', '0')).toBe('0');
-  expect(add('0.1', '0.3')).toBe('0.4');
-  expect(add('0.94', '0.38')).toBe('1.32');
-  expect(add('94', '38')).toBe('132');
-  expect(add('-94', '-38')).toBe('-132');
-  //expect(longAdd('-1', '0')).toBe('0');
-});
 
 test("bigNumber-plus", () => {
+  const a = '111111111111119';
+  const b = '11111111111111112';
+  expect(plus(a, b)).toBe('11222222222222231');
+  expect(plus('0', '0')).toBe('0');
+  expect(plus('0.1', '0.3')).toBe('0.4');
+  expect(plus('0.94', '0.38')).toBe('1.32');
+  expect(plus('94', '38')).toBe('132');
+  expect(plus('-94', '-38')).toBe('-132');
+  expect(plus('-94', '38')).toBe('-56');
+  expect(plus('94', '-38')).toBe('56');
+  
+});
+
+test("bigNumber-times", () => {
   const a = "123", b = "111";
-  expect(plus(a, b)).toBe('13653');
-  expect(plus("0", "0")).toBe('0');
-  expect(plus("88", "8")).toBe('704');
-  expect(plus("999", "999")).toBe('998001');
-  expect(plus("99.9", "9.99")).toBe('998.001');
-  expect(plus("12.3", "11.1")).toBe('136.53');
-  expect(plus("-12.3", "11.1")).toBe('-136.53');
+  expect(times(a, b)).toBe('13653');
+  expect(times("0", "0")).toBe('0');
+  expect(times("88", "8")).toBe('704');
+  expect(times("999", "999")).toBe('998001');
+  expect(times("99.9", "9.99")).toBe('998.001');
+  expect(times("12.3", "11.1")).toBe('136.53');
+  expect(times("-12.3", "11.1")).toBe('-136.53');
+})
+
+//TODO
+test('bigNumber-minus', () => {
+  expect(minus('999', '998')).toBe('1');
+  expect(minus('888', '999')).toBe('-111');
+  expect(minus('9899', '9999')).toBe('-100');
+  expect(minus('-100', '100')).toBe('-200');
+  expect(minus('-100', '-100')).toBe('0');
+  expect(minus('100', '-100')).toBe('200');
+  expect(minus('100', '100')).toBe('0');
+  expect(minus('0', '0')).toBe('0');
+  expect(minus('38', '94')).toBe('-56');
+  expect(minus('38.4', '-94.3')).toBe('132.7');
+  expect(minus('100', '99')).toBe('1');
+  expect(minus('99', '100')).toBe('-1');
+  expect(minus('100.99', '99.11')).toBe('1.88');
+  expect(minus('-38.4', '-94.3')).toBe('55.9');
+  expect(minus('38.4', '94.3')).toBe('-55.9');
 })
